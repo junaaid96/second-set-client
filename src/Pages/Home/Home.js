4722b7f0db9ac33933ed50ec3b53061fe51c8ae0
Md. Junaidul Islam <info.junaidulislam@gmail.com>
2022-11-30 07:29:16 +0600

    extra section added

--- src/Pages/Home/Home.js
+++ src/Pages/Home/Home.js
@@ -1,11 +1,13 @@
 import React from "react";
 import Banner from "./Banner/Banner";
 import Categories from "./Categories/Categories";
+import MockUpScreen from "./MockUpScrren/MockUpScreen";
 
 const Home = () => {
     return (
         <>
             <Banner />
+            <MockUpScreen />
             <Categories />
         </>
     );
---
+++ src/Pages/Home/MockUpScrren/MockUpScreen.js
@@ -0,0 +1,18 @@
+import React from "react";
+
+const MockUpScreen = () => {
+    return (
+        <div className="text-center mt-10">
+            <div className="mockup-phone border-primary">
+            <div className="camera"></div>
+            <div className="display">
+                <div className="artboard artboard-demo phone-1">
+                    Hi, New user
+                </div>
+            </div>
+        </div>
+        </div>
+    );
+};
+
+export default MockUpScreen;
