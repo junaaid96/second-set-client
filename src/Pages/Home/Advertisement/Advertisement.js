25622dfe838113eb276338da3c866c84bb2c719f
Md. Junaidul Islam <info.junaidulislam@gmail.com>
2022-12-01 02:21:45 +0600

    info corrected

--- src/Pages/Home/Advertisement/Advertisement.js
+++ src/Pages/Home/Advertisement/Advertisement.js
@@ -6,14 +6,17 @@ const Advertisement = () => {
             <h1 className="text-3xl mb-6 text-center">Advertisements</h1>
             <div className="card w-96 bg-base-100 shadow-xl">
                 <figure>
-                    <img src="https://placeimg.com/400/225/arch" alt="Shoes" />
+                    <img
+                        src="https://consumer.huawei.com/content/dam/huawei-cbg-site/common/mkt/plp/phones/new-phones/model-banner/p50-pro-black.png"
+                        alt="phone"
+                    />
                 </figure>
                 <div className="card-body">
                     <h2 className="card-title">
-                        Shoes!
+                        dummy phone
                         <div className="badge badge-secondary">NEW</div>
                     </h2>
-                    <p>If a dog chews shoes whose shoes does he choose?</p>
+                    <p>Price: 0</p>
                 </div>
             </div>
         </div>
