f835d09d1e1e9e6033c63804af03c8e46d7eef4b
Md. Junaidul Islam <info.junaidulislam@gmail.com>
2022-12-02 11:03:35 +0600

    route error handling

--- src/Pages/Shared/Reloading/Reloading.js
+++ src/Pages/Shared/Reloading/Reloading.js
@@ -2,10 +2,10 @@ import React from "react";
 
 const Reloading = () => {
     return (
-        <div className="text-center font-bold">
+        <div className="text-center font-bold text-3xl mt-12">
             Something went wrong. Try reloading this page. If reloading does not
-            solve the problem, The route you requested does not exist.  <br />
-            Sorry for the inconvenience.
+            solve the problem, The route you requested does not exist. Sorry for
+            the inconvenience.
         </div>
     );
 };
